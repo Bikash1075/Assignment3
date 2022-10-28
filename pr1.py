a095dad785ab679be6bd98db1485d3eccab5a195
Biki <102693150+Bikash1075@users.noreply.github.com>
2022-10-29 00:23:42 +0530

    Add files via upload

--- pr1.py
+++ pr1.py
@@ -1,6 +1,17 @@
+# Write a Python function to sum all the numbers in a list.
+# Sample List : (8, 2, 3, 0, 7)
+# Expected Output : 20
+# Explanation:
+# Summation should like 8+2+3+0+7 = 20
 
 
-
+def addition(l):
+    sum=0
+    for i in l:
+        sum=sum+i
+    return sum
+l=[8, 2, 3, 0, 7]
+print (addition(l))
 
 
 
